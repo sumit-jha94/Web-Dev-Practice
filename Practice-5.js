e115443e75542bd6fc4f25ba4f91a1d9b2999085
sumit-jha94 <sumitshine14@gmail.com>
2023-12-23 13:16:12 +0530

    practice-5 solved

---
+++ Practice-5.js
@@ -0,0 +1,23 @@
+// Swaping 2 variables
+
+let a = parseFloat( prompt("Enter The Number 1"));
+let b = parseFloat( prompt("Enter The NUmber 2"));
+
+console.log(`The Older Value of A is ${a}`);
+console.log(`The Older Value of b is ${b}`);
+// let a = 5;
+// let b = 4;
+
+let c;
+
+c = a;
+a = b;
+b = c;
+
+// b = a;
+// a = b;
+
+
+
+console.log(`The New A = ${a}`)
+console.log(`The New B = ${b}`)
