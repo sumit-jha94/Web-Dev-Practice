4bdf4954f2cab8df523d1b3a23758f52d17fa6fc
sumit-jha94 <sumitshine14@gmail.com>
2023-12-25 21:45:49 +0530

    question 10 solved

---
+++ Practice-10.js
@@ -0,0 +1,10 @@
+let num = parseInt(prompt("Enter The Number"));
+
+if (num<0) {
+    console.log("Number is negative")
+} else if (num == 0) {
+    console.log("Number is zero")
+} else {
+    console.log("Number is positive")
+}
+
